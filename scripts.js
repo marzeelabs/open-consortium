926521899919a3e81cd3e537d395c2f3d8225ad7
Nuno Veloso <nunoveloso18@gmail.com>
2012-05-23 00:23:56 +0200

    basic assets pulled in

---
+++ scripts.js
@@ -0,0 +1,35 @@
+$('.carousel').carousel({
+  interval: 2000
+})
+
+
+/* jquery.tweet */
+jQuery(function($){
+  $("#tweet").tweet({
+    username: "openconsortium",
+    page: 1,
+    avatar_size: 32,
+    count: 1,
+    loading_text: "loading ..."
+  }).bind("loaded", function() {
+    var ul = $(this).find(".tweet_list");
+    var ticker = function() {
+      setTimeout(function() {
+        ul.find('li:first').animate( {marginTop: '-4em'}, 500, function() {
+          $(this).detach().appendTo(ul).removeAttr('style');
+        });
+        ticker();
+      }, 5000);
+    };
+    ticker();
+  }).hide();
+});
+
+
+$('#twitter-url').popover({
+  placement: 'bottom',
+  content: function() {
+    return $("#tweet").html();
+  }
+});
+
