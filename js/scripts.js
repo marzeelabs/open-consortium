f5f86aeca7f50798beae231f4b464e79ecf91cfe
Nuno Veloso <nunoveloso18@gmail.com>
2012-05-22 22:30:12 +0200

    added latest tweets ticker with jquery.tweet

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,25 @@
 $('.carousel').carousel({
   interval: 2000
 })
+
+/* jquery.tweet */
+jQuery(function($){
+  $("#tweet").tweet({
+    username: "nunoveloso",
+    page: 1,
+    avatar_size: 32,
+    count: 5,
+    loading_text: "loading ..."
+  }).bind("loaded", function() {
+    var ul = $(this).find(".tweet_list");
+    var ticker = function() {
+      setTimeout(function() {
+        ul.find('li:first').animate( {marginTop: '-4em'}, 500, function() {
+          $(this).detach().appendTo(ul).removeAttr('style');
+        });
+        ticker();
+      }, 5000);
+    };
+    ticker();
+  });
+});
